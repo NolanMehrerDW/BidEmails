c97272e87183457299daa928eb786925f83440c7
nolanyoung90 <nolan@mehrer.com>
2024-09-29 10:58:41 -0700

    Updated pip os-sys install

--- BidEmails.bat
+++ BidEmails.bat
@@ -1,6 +1,6 @@
 @echo on
 git pull https://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@github.com/NolanMehrerDW/BidEmails.git
 python -m pip install --upgrade pip
-python -m pip install spacy pywin32 regex pandas os datetime random
+python -m pip install spacy pywin32 regex pandas os-sys datetime random
 Python "automation.py"
 pause
