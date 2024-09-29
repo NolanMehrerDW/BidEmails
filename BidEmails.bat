e81c1c65edc91a9ddcc4410946fdc2dfeead17c9
nolanyoung90 <nolan@mehrer.com>
2024-09-29 10:52:56 -0700

    V.09292024
    Updated batch launcher and pip installer

--- BidEmails.bat
+++ BidEmails.bat
@@ -1,6 +1,6 @@
 @echo on
 git pull https://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@github.com/NolanMehrerDW/BidEmails.git
 python -m pip install --upgrade pip
-python -m pip install pywin32 datetime scikit-learn nltk transformers
-Python "main10.py"
+python -m pip install spacy pywin32 re pandas os datetime random
+Python "automation.py"
 pause
